3e922c74b104811db4ece8c6754b01d2e2b4c245
WSINTRA <will.simpson85@gmail.com>
2019-03-21 13:03:16 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,25 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
+    
+  	comment = Comment.create(comment_params)
+  	if params[:user_attributes][:username] != "new username"
+  		new_user = User.create(new_user_params)
+  		comment.user_id = new_user.id
+  		comment.save
+  		
+  	end
+    
+
     redirect_to comment.post
   end
 
   private
-
+  
+  def new_user_params
+  	params.require(:user_attributes).permit(:username)
+  end
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @users = User.all
   end
 
   def index
@@ -8,15 +9,22 @@ class PostsController < ApplicationController
   end
 
   def new
+
     @post = Post.new
   end
 
   def create
     post = Post.create(post_params)
+    Category.find_or_create_by(category_params[:categories])
+    post.categories << Category.last
     redirect_to post
   end
 
   private
+
+  def category_params
+    params.require(:post).permit(categories:[:name])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
